62e155d913e0362638f14fd7e56d22dc47e16150
moonstar-x <christian.lopez99@outlook.com>
2023-01-05 06:14:25 -0500

    Removed unnecessary script files.

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm -p 7474:7474 -p 7687:7687 --env=NEO4J_AUTH=none peptides/db
-
-# docker run -it --rm -p 7474:7474 -p 7687:7687 --env=NEO4J_AUTH=none -v $PWD/data:/data -v $PWD/logs:/logs neo4j:3.5
