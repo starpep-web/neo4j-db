0321c610ee028a518c78f8acb294c27bcb2390c1
moonstar-x <christian.lopez99@outlook.com>
2023-07-31 23:13:03 -0500

    Updated docker update scripts.

--- update-scripts/docker-scripts/write.sh
+++ update-scripts/docker-scripts/write.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec -it local-neo4j-to-modify cp -r /data/databases/graph.db/. /output && zip starPep.db.zip -r starPep.db/*
+cd ../..
+docker exec -it local-neo4j-to-modify cp -r /data/databases/graph.db/. /output && zip starPep.db.zip -r starPep.db/* && rm -rf starPep.db
